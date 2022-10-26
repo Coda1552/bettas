39abda90bc6bfdf7d7a933711719796e46a61db7
baguchan <shurf012@gmail.com>
2022-10-26 10:24:02 +0900

    moss ball block too!

--- src/main/java/teamfusion/bettas/entity/BettaFishEntity.java
+++ src/main/java/teamfusion/bettas/entity/BettaFishEntity.java
@@ -194,6 +194,9 @@ public class BettaFishEntity extends AbstractFish implements Bucketable {
                         if (this.level.getBlockState(blockpos$mutable).is(BettasBlocks.MOSS_BALL.get())) {
                             flag = true;
                             mossCount += this.level.getBlockState(blockpos$mutable).getValue(MossBallBlock.BALLS);
+                        } else if (this.level.getBlockState(blockpos$mutable).is(BettasBlocks.MOSS_BALL_BLOCK.get())) {
+                            flag = true;
+                            mossCount += 8;
                         }
                     }
                 }
